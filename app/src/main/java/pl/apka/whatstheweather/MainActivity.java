5bca8d0dd5af86be59a7915cc37617a4a48b8149
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-12-27 15:36:34 +0100

    java funcitions and layout added

--- app/src/main/java/pl/apka/whatstheweather/MainActivity.java
+++ app/src/main/java/pl/apka/whatstheweather/MainActivity.java
@@ -1,13 +1,111 @@
 package pl.apka.whatstheweather;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView resultTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        resultTextView = findViewById(R.id.resultTextView);
+
     }
+
+    public void getWeather(View view){
+
+        String city = editText.getText().toString();
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://openweathermap.org/data/2.5/weather?q="+city+"&appid=b6907d289e10d714a6e88b30761fae22");
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result ="";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current  = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                String message = "";
+
+                for (int i = 0; i < arr.length(); i++){
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = jsonPart.getString("main");
+                    String desc =jsonPart.getString("description");
+
+                    if (!main.equals("") && !desc.equals("")){
+                        message += main + ": " + desc + "\r\n";
+                    }
+                }
+
+                if (!message.equals("")) {
+                    resultTextView.setText(message);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+
 }
